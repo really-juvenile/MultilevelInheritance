4832291d30b2fd71e0afcbd26f6ab046a19efad0
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-07-17 08:24:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilevelInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Eat();
+            myDog.Walk();
+            myDog.Bark();
+        }
+        public class Animal //this is  the base class or parent class
+        {
+            public void Eat()
+            {
+                Console.WriteLine("The animal is eating.");
+            }
+        }
+
+        public class Mammal : Animal //mammal class has been derived from animal class
+        {
+            public void Walk()
+            {
+                Console.WriteLine("The mammal is walking.");
+            }
+        }
+
+        public class Dog : Mammal //the derived class has been derived into  child class .
+            //this is also called as heirarchical inheritance.
+            //here dog is been derived from Mammal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("The dog is barking.");
+            }
+        }
+    }
+}
